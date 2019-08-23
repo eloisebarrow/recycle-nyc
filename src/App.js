a49e89cc734fce3b6b05c94c61349a9719552112
Eloise Barrow <eloisebarrow@Eloises-Air.home>
2019-08-22 21:10:01 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: [],
+  }
+
+  makeApiCall = async () => {
+    const response = await axios.get('https://data.cityofnewyork.us/resource/sxx4-xhzg.json')
+    const data = response.data;
+
+    this.setState({
+      data
+    }, () => {
+      console.log('this is data from API call', this.state.data)
+    })
+  }
+
+  componentDidMount() {
+    this.makeApiCall();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Recycle NYC</h1>
+        <h3>Waste Not, Want Not: Find a public recycling bin near you!</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
