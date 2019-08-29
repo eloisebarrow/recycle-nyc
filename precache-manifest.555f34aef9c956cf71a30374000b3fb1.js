d9ff6bd0c797cbc36f513fa2ba6de77043cc6b99
Eloise Barrow <eloisebarrow@Eloises-MacBook-Air.local>
2019-08-29 10:16:24 -0400

    Updates

---
+++ precache-manifest.555f34aef9c956cf71a30374000b3fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f593846c768b9181e1565b45728958",
+    "url": "/eloisebarrow/recycle-nyc/index.html"
+  },
+  {
+    "revision": "bc70a6860cdafb2a1e2a",
+    "url": "/eloisebarrow/recycle-nyc/static/css/main.fa39e953.chunk.css"
+  },
+  {
+    "revision": "3cc3e20d53344c8b54b2",
+    "url": "/eloisebarrow/recycle-nyc/static/js/2.4c39f5cc.chunk.js"
+  },
+  {
+    "revision": "bc70a6860cdafb2a1e2a",
+    "url": "/eloisebarrow/recycle-nyc/static/js/main.741ccc0e.chunk.js"
+  },
+  {
+    "revision": "21bea30c7496c997617f",
+    "url": "/eloisebarrow/recycle-nyc/static/js/runtime~main.d3e7a5b0.js"
+  }
+]);
